4a1abec174b8a07cedaccc7da8272d997219e4b1
lijianqing <lijianqing>
2020-04-23 17:20:00 +0800

    modify readme and holiday.py

--- holiday.py
+++ holiday.py
@@ -6,6 +6,7 @@ import re
 from bs4 import BeautifulSoup
 import datetime
 import time
+import json
 
 def get_url_html(url, encoding=None):
     r = requests.get(url)
@@ -20,14 +21,14 @@ def get_holiday_url(year, html_doc):
     for td in td_list:
         link = td.find("a")
         title = "".join(link.contents)
-        search = re.search(ur'国务院办公厅关于(\d+)年部分节假日安排的通知', title)
+        search = re.search('国务院办公厅关于(\d+)年部分节假日安排的通知', title)
         if search and int(search.group(1)) == year:
             return link["href"]
     return None
 
 
 def match_holiday(html_doc, year=datetime.datetime.now().year + 1):
-    html_doc = re.sub(r'<(/|)br(/|)>', '\n', html_doc) # 将标签换行改为换行符。
+    html_doc = re.sub(r'<(/|)br(/|)>', '\n', html_doc.decode('utf-8')) # 将标签换行改为换行符。
     html_doc = html_doc.replace('　', '')  # 过滤掉全角空格
     soup = BeautifulSoup(html_doc, "lxml")
     td_content = soup.find("td", {"class": "b12c"})
@@ -35,30 +36,27 @@ def match_holiday(html_doc, year=datetime.datetime.now().year + 1):
     html = html.split(u"\n")
     holiday = []
     for line in html:
-        mat = re.search(ur"^[一二三四五六七八九十]*?、(.*?)$", line.strip())
+        mat = re.search("^[一二三四五六七八九十]*?、(.*?)$", line.strip())
         if mat:
             desc = mat.group(1)
             name, plan = desc.split(u"：", 2)
-            holiday_mat = re.search(ur"(?P<holiday>\d+月\d+日.*?)放假", plan)
-            workday_mat = re.search(ur"[，。](?P<workday>\d+月\d+日[^，。]*?)上班", plan)
+            holiday_mat = re.search("(?P<holiday>\d+月\d+日.*?)放假", plan)
+            workday_mat = re.search("[，。](?P<workday>\d+月\d+日[^，。]*?)上班", plan)
             holiday_desc = holiday_mat.group("holiday") if holiday_mat else ""
             workday_desc = workday_mat.group("workday") if workday_mat else ""
             holiday_parse = parse_date(holiday_desc, year)
             workday_parse = parse_date(workday_desc, year)
             if not holiday_parse:
                 raise Exception(u"解析{}年节日{}假期失败！".format(year, name))
-            holiday.append({
-                "name": name,
-                "holiday": holiday_parse,
-                "workday": workday_parse,
-            })
+            for i in range(len(holiday_parse)):
+                holiday.append(holiday_parse[i])
 
     return holiday
 
 
 def parse_date(desc, year):
     mat_days = re.findall(
-        ur"(?P<from_month>\d+)月(?P<from_day>\d+)日至(?P<to_month>\d+)月(?P<to_day>\d+)日", desc)
+        "(?P<from_month>\d+)月(?P<from_day>\d+)日至(?P<to_month>\d+)月(?P<to_day>\d+)日", desc)
     if mat_days:
         days = []
         month_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -76,7 +74,7 @@ def parse_date(desc, year):
                     days.append(get_time_stamp(year, month, day))
                 day_begin = 1
         return days
-    mat_days = re.findall(ur"(?P<month>\d+)月(?P<from>\d+)日至(?P<to>\d+)日", desc)
+    mat_days = re.findall("(?P<month>\d+)月(?P<from>\d+)日至(?P<to>\d+)日", desc)
     if mat_days:
         days = []
         for mat in mat_days:
@@ -84,7 +82,7 @@ def parse_date(desc, year):
             for day in range(int(from_day), int(to_day) + 1):
                 days.append(get_time_stamp(year, int(month), day))
         return days
-    mat_days = re.findall(ur"(?P<month>\d+)月(?P<from>\d+)日", desc)
+    mat_days = re.findall("(?P<month>\d+)月(?P<from>\d+)日", desc)
     if mat_days:
         days = []
         for mat in mat_days:
@@ -105,8 +103,18 @@ def get_holiday(year):
     url = "http://sousuo.gov.cn/list.htm?q=&n=15&t=paper&childtype=&subchildtype=gc189&pcodeJiguan=%E5%9B%BD%E5%8A%9E%E5%8F%91%E6%98%8E%E7%94%B5&pcodeYear=&pcodeNum=&location=%25E7%25BB%25BC%25E5%2590%2588%25E6%2594%25BF%25E5%258A%25A1%25E5%2585%25B6%25E4%25BB%2596&sort=pubtime&searchfield=title%3Acontent%3Apcode%3Apuborg%3Akeyword&title=&content=&pcode=&puborg=&timetype=timeqb&mintime=&maxtime="    
     holiday_url = get_holiday_url(year, get_url_html(url))
     if holiday_url:
-        return match_holiday(get_url_html(holiday_url, 'utf-8'), year)
+        return {"holiday": match_holiday(get_url_html(holiday_url, 'utf-8'), year)}
     return None
 
+def write_file(data, file):
+    with open(file, "w",  encoding='utf-8') as f:
+        f.write(str(data))
+        print("write data to %s success" %file)
+
 if __name__ == "__main__":
-    print get_holiday(datetime.datetime.now().year)
+    year = datetime.datetime.now().year
+    result_json = get_holiday(year)
+    if(result_json):
+        write_file(json.dumps(result_json), str(year)+".json")
+    else:
+        print("error read holiday data from internet")
--- readme.md
+++ readme.md
@@ -1,31 +1,32 @@
-# 国家节假日解析爬虫
-可用于解析获取国家节假日公布页面的节假日安排。
+# 国家节假日解析爬虫(crawler of chinese holiday)
 
-## 使用示例
+可用于解析获取国家节假日公布页面的节假日安排。(It can be used to analyze and obtain the holiday arrangement of the chinese national holiday announcement page)
+
+## 使用示例(demo)
+
 ```python
 from holiday import get_holiday
 from datetime import datetime
 
-# 得到当前年节假日
+# 得到当前年节假日(get holiday of this year)
 current_year_holiday = get_holiday(datetime.now().year)
 
 ```
 
-返回节假日名、假日和补休日时间戳：
+返回该年所有节假日的时间戳(return every holiday of China this year, but not included weekend)：
+
 ```json
-[{
-    "name": "春节",
-    "holiday": [
-        1518624000, 1518710400, 
-        1518796800, 1518883200, 
-        1518969600, 1519056000, 
-        1519142400
-    ],
-    "workday": [
-        1518278400, 1519401600
-    ]
-}]
+
+{"holiday": [1293811200, 1293897600, 1293984000, 1296576000, 1301760000, 1301846400, 1301932800, 1304092800, 1304179200, 1304265600, 1307116800, 1307203200, 1307289600, 1315584000, 1315670400, 1315756800, 1317398400, 1317484800, 1317571200, 1317657600, 1317744000, 1317830400, 1317916800]}
 ```
 
-## 注意
-建议作为每年执行的脚本调用，而非实时API。
+## 注意(attention)
+
+建议作为每年执行的脚本调用，而非实时API。(It is recommended to call every year rather than in real time)
+
+## 主要修改点(major modify)
+
+1. python2 -> python3(python3.5)
+2. 增加了一个写入json的例子，可以看一下，同时把采集到的json文件也上传了(demo for json file)
+
+## fork from [chinese_holiday_spider_module](https://github.com/imlinhanchao/chinese_holiday_spider_module)
